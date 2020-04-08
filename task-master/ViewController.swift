ecc01e89d79be13d5b9026483b2b7140162b0295
yohan <yohan3287@binus.ac.id>
2020-04-08 15:06:28 +0700

    coba push

---
+++ task-master/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  task-master
+//
+//  Created by Yohan Wongso on 08/04/20.
+//  Copyright © 2020 Yohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//ashahjsjsabjk
