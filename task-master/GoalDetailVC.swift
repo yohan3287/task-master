91351a03504ad00859ba440156ef40e6fe0deaf7
yohan <yohan3287@binus.ac.id>
2020-04-11 02:11:10 +0700

    date picker sudah jalan di GoalDetailVC

--- task-master/GoalDetailVC.swift
+++ task-master/GoalDetailVC.swift
@@ -5,6 +5,7 @@
 //  Created by Yohan Wongso on 09/04/20.
 //  Copyright © 2020 Yohan. All rights reserved.
 //
+// Referensi : https://www.swiftdevcenter.com/uidatepicker-as-input-view-to-uitextfield-swift/
 
 import UIKit
 
@@ -43,76 +44,33 @@ var goal02 = goalTmp(goalName: "World class developer", // dummy data
                         collaborator(collaboratorName: "Titan", collaboratorProgress: 100)])
 var goalList: [goalTmp] = []
 
-class GoalDetailVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+class GoalDetailVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var titleVC: UINavigationItem!
     @IBOutlet weak var goalNameTextField: UITextField!
     @IBOutlet weak var goalTimeStartTextField: UITextField!
     @IBOutlet weak var goalTimeEndTextField: UITextField!
     @IBOutlet weak var missionsListTableView: UITableView!
-   
-        var datePickerView  : UIDatePicker = UIDatePicker()
-        var datePickerView2 : UIDatePicker = UIDatePicker()
-        @objc func dismissKeyboard() {
-             //Causes the view (or one of its embedded text fields) to resign the first responder status.
-             view.endEditing(true)
-         }
-         func didTap(sender: UITapGestureRecognizer) {
-            _ = sender.location(in: view)
-         }
-         
-        @objc func datePickerChanged(sender: UIDatePicker) {
-                             let formatter = DateFormatter()
-                         
-            
-                        formatter.dateFormat = "yyyy MM dd"
-                       
-                            goalTimeStartTextField.text = formatter.string(from: sender.date)
-                          
-                       }
     
-    @objc func datePickerChanged2(sender: UIDatePicker) {
-                          
-                            let formatter2 = DateFormatter()
-           
-                   
-                       formatter2.dateFormat = "yyyy MM dd"
-                         
-                           goalTimeEndTextField.text = formatter2.string(from: sender.date)
-           
-                      }
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-            let datePicker = UIDatePicker()
-            let datePicker2 = UIDatePicker()
-            textField.inputView = datePicker
-            datePicker.addTarget(self, action: #selector(datePickerChanged(sender:)), for: .valueChanged)
-            datePicker2.addTarget(self, action: #selector(datePickerChanged2(sender:)), for: .valueChanged)
+    @objc func tapDoneFromGoalTimeStartDatePicker() { // controller of done button from goal time start date picker
+        if let datePicker = self.goalTimeStartTextField.inputView as? UIDatePicker {
+            let dateformatter = DateFormatter()
+            dateformatter.dateFormat = "dd MMM yyyy"
+//            dateformatter.dateStyle = .medium
+            self.goalTimeStartTextField.text = dateformatter.string(from: datePicker.date)
         }
-//        @objc func donePick(){
-//
-//               let dateFormatter = DateFormatter()
-//               dateFormatter.dateFormat = "yyyy-MM-dd"
-//               goalTimeStartTextField.text = dateFormatter.string(from: datePickerView.date)
-//               self.view.endEditing(true)
-//           }
-//
-//        func displayDate(){
-//                  let toolbar = UIToolbar()
-//                  toolbar.sizeToFit()
-//
-//                  let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePick))
-//                  toolbar.setItems([doneBtn], animated: false)
-//
-//                  goalTimeStartTextField.inputAccessoryView = toolbar
-//
-//                  goalTimeStartTextField.inputView = datePickerView
-//              }
-          
-          
-
-       
-    
-
+        self.goalTimeStartTextField.resignFirstResponder()
+    }
+        
+    @objc func tapDoneFromGoalTimeEndDatePicker() { // controller of done button from goal time end date picker
+        if let datePicker = self.goalTimeEndTextField.inputView as? UIDatePicker {
+            let dateformatter = DateFormatter()
+            dateformatter.dateFormat = "dd MMM yyyy"
+//            dateformatter.dateStyle = .medium
+            self.goalTimeEndTextField.text = dateformatter.string(from: datePicker.date)
+        }
+        self.goalTimeEndTextField.resignFirstResponder()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -121,34 +79,10 @@ class GoalDetailVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         goalList.append(goal01)
         goalList.append(goal02)
         
-        goalNameTextField.text = goal01.goalName
-        goalTimeStartTextField.text = goal01.goalTimeStart
-        goalTimeEndTextField.text = goal01.goalTimeEnd
-    
-    goalTimeStartTextField.delegate = self
-        goalTimeEndTextField.delegate = self
-        datePickerView2.datePickerMode = UIDatePicker.Mode.date
-           datePickerView.datePickerMode = UIDatePicker.Mode.date
-           goalTimeStartTextField.inputView = datePickerView
-        goalTimeEndTextField.inputView = datePickerView2
-      
-           datePickerView.addTarget(self, action:
-               Selector(("datePickerChanged:")), for: UIControl.Event.valueChanged)
-        
-           datePickerView2.addTarget(self, action:
-           Selector(("datePickerChanged2:")), for: UIControl.Event.valueChanged)
-        
-            let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-               view.addGestureRecognizer(tap)
-           let datePicker2 = UIDatePicker()
-           let datePicker = UIDatePicker()
-             
-               datePickerView.addTarget(self, action: #selector(datePickerChanged(sender:)), for: .valueChanged)
-               datePickerView2.addTarget(self, action: #selector(datePickerChanged2(sender:)), for: .valueChanged)
-               goalTimeStartTextField.inputView = datePicker
-               goalTimeEndTextField.inputView = datePicker2
-
+        self.goalTimeStartTextField.setInputViewDatePicker(target: self, selector: #selector(tapDoneFromGoalTimeStartDatePicker))
+        self.goalTimeEndTextField.setInputViewDatePicker(target: self, selector: #selector(tapDoneFromGoalTimeEndDatePicker))
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return goal01.missionsList.count
     }
---
+++ task-master/UITextField+InputView.swift
@@ -0,0 +1,34 @@
+//
+//  UITextField+InputView.swift
+//  task-master
+//
+//  Created by Yohan Wongso on 11/04/20.
+//  Copyright © 2020 Yohan. All rights reserved.
+//
+// Referensi : https://www.swiftdevcenter.com/uidatepicker-as-input-view-to-uitextfield-swift/
+
+import Foundation
+import UIKit
+extension UITextField {
+    
+    func setInputViewDatePicker(target: Any, selector: Selector) {
+        // Create a UIDatePicker object and assign to inputView
+        let screenWidth = UIScreen.main.bounds.width
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
+        datePicker.datePickerMode = .date
+        self.inputView = datePicker
+        
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel))
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
+        toolBar.setItems([cancel, flexible, barButton], animated: false)
+        self.inputAccessoryView = toolBar
+    }
+    
+    @objc func tapCancel() {
+        self.resignFirstResponder()
+    }
+    
+}
