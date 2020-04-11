6b894922f5df1a50cd9a4cdc138cac52e3460187
Marvin Marcio <marvinmarcio999@gmail.com>
2020-04-11 15:32:56 +0700

    add "Add Goals View" and "AddGoalsVC" file (non-logic) need 2 fix navbar

---
+++ task-master/AddGoalsVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddGoalsVC.swift
+//  task-master
+//
+//  Created by Marvin Marcio on 11/04/20.
+//  Copyright © 2020 Yohan. All rights reserved.
+//
+
+import UIKit
+
+class AddGoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
